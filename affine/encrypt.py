d547548e9fa0ce4a677849cb616a7de7c66114fc
ivan iliev <vanishonko@gmail.com>
2022-01-11 09:25:19 +0200

    working encryption

---
+++ affine/encrypt.py
@@ -0,0 +1,56 @@
+import math
+symbols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def encrypt(key_, msg_, encrypting_):
+    k1 = math.floor(key_ / 100)
+    while k1 > 26:
+        k1 -= 26
+    k2 = key_ % 100
+    if encrypting_:
+        newMsg = multiplicative(k2, caesar(k1, msg_, encrypting_), encrypting_)
+    else:
+        newMsg = caesar(k1, multiplicative(k2, msg_, encrypting_), encrypting_)
+    return newMsg
+
+def caesar(key_, msg, encrypting_):
+    if not encrypting_:
+        key_ = 26 - key_
+    newMsg = ''
+    for i in msg:
+        isInAlphabet = False
+        currentLetter = i
+        for j in symbols:
+            if currentLetter.lower() == j:
+                isInAlphabet = True
+                break
+        newSymb = currentLetter
+        if isInAlphabet:
+            for v in range(26):
+                if currentLetter.lower() == symbols[v]:
+                    k = v
+                    break
+            n = k + key_
+            if n > 25:
+                n -= 26
+            newSymb = symbols[n]
+            if currentLetter != currentLetter.lower():
+                newSymb = newSymb.upper()
+        newMsg += newSymb
+    return newMsg
+
+def multiplicative(key_, msg, encrypting_):
+    newMsg = ''
+    for i in msg:
+        if i == ' ':
+            newMsg += ' '
+        else:
+            curIndex = symbols.index(i)
+            newSym = symbols[ curIndex * key_ % 26]
+            newMsg += newSym
+    return newMsg
+
+def bruteforce(_msg):
+    messages = []
+    for i in range(150, 10000):
+        messages.append(encrypt(i, _msg, False))
+    return messages
